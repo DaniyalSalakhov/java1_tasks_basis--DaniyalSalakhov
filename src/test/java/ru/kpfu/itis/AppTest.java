bf2eb14ceb92237e57bb3032d8c24d2ad9b0d970
Daniyal Salakhov <71236285+DaniyalSalakhov@users.noreply.github.com>
2020-11-07 20:57:11 +0300

    Initial commit

---
+++ src/test/java/ru/kpfu/itis/AppTest.java
@@ -0,0 +1,14 @@
+package ru.kpfu.itis;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Just basic junit test class example with test that is always passing.
+ */
+public class AppTest {
+    @Test
+    public void alwaysPass(){
+        Assertions.assertTrue(true, "Always passing test.");
+    }
+}
